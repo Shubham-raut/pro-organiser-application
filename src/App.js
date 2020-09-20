4c26245589153d91dd683166fdea7531c1fa50cf
Shubham-raut <123shubhamraut@gmail.com>
2020-09-20 14:12:13 +0530

    project reformed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Authentication from './Context/Authentication';
-import Header from './Components/Header/Header';
-import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';
-import Signup from './Pages/Signup/Signup';
-import Board from './Pages/Board/Board';
-import './App.css';
-import PrivateRoute from './PrivateRoute';
-import CreateBoard from './Pages/CreateBoard/CreateBoard';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route} 
+from 'react-router-dom';
+import CreateBoard from './components/CreateBoard/CreateBoard';
+import Home from './components/Home/Home';
+import Main from './components/Main/Main';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <Authentication>
-      <BrowserRouter>
-        <Header />
+    <Router>
+      <NavBar/>
+       <div>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/createboard" component={CreateBoard} />
-          <Route path="/board/:boardid" component={Board} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/login" component={Login} />
-          <Route exact path="*" component={Home} />
+          <Route path="/" exact component={Home} />
+          <Route path="/CreateBoard" component={CreateBoard}/>
+          <Route path="/:id" component={Main} />                                                             
         </Switch>
-      </BrowserRouter>
-    </Authentication>
+       </div>
+    </Router>
+
   );
 }
 
